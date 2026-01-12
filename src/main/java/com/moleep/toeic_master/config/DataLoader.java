817c941969a2839ff3ff4df666b7c843d8d83f56
amadeus <amadeus@amadeusui-MacBookPro.local>
2026-01-12 17:04:44 +0900

    학교 데이터 추가

--- src/main/java/com/moleep/toeic_master/config/DataLoader.java
+++ src/main/java/com/moleep/toeic_master/config/DataLoader.java
@@ -60,8 +60,60 @@ public class DataLoader implements CommandLineRunner {
                         .address("대전광역시 서구 둔산북로 232-11 (둔산동, 문정중학교)")
                         .latitude(new BigDecimal("36.35512930"))
                         .longitude(new BigDecimal("127.40053470"))
+                        .build(),
+
+                // ===== 추가된 학교 =====
+
+                School.builder()
+                        .name("송촌중")
+                        .address("대전광역시 대덕구 계족산로 110")
+                        .latitude(new BigDecimal("36.36396610"))
+                        .longitude(new BigDecimal("127.44280800"))
+                        .build(),
+
+                School.builder()
+                        .name("대전봉명중")
+                        .address("대전광역시 유성구 계룡로132번길 71 (봉명동, 대전봉명중학교)")
+                        .latitude(new BigDecimal("36.34943980"))
+                        .longitude(new BigDecimal("127.34438890"))
+                        .build(),
+
+                School.builder()
+                        .name("지족중")
+                        .address("대전광역시 유성구 노은동로 193")
+                        .latitude(new BigDecimal("36.37758990"))
+                        .longitude(new BigDecimal("127.32006050"))
+                        .build(),
+
+                School.builder()
+                        .name("하기중")
+                        .address("대전광역시 유성구 송림로48번길 6-30 (하기동, 대전하기중학교)")
+                        .latitude(new BigDecimal("36.38563140"))
+                        .longitude(new BigDecimal("127.32179700"))
+                        .build(),
+
+                School.builder()
+                        .name("대전여중")
+                        .address("대전광역시 중구 보문로230번길 69")
+                        .latitude(new BigDecimal("36.32483650"))
+                        .longitude(new BigDecimal("127.42579660"))
+                        .build(),
+
+                School.builder()
+                        .name("문화여중")
+                        .address("대전광역시 중구 문화로218번길 63")
+                        .latitude(new BigDecimal("36.31306560"))
+                        .longitude(new BigDecimal("127.40905350"))
+                        .build(),
+
+                School.builder()
+                        .name("충남여중")
+                        .address("대전광역시 중구 동서대로1352번길 33")
+                        .latitude(new BigDecimal("36.33007650"))
+                        .longitude(new BigDecimal("127.41101070"))
                         .build()
         );
+
 
         schoolRepository.saveAll(schools);
         log.info("Loaded {} schools", schools.size());
